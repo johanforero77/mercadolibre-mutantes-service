ca2d13fc7f37be0a3fbdd54dbd80f62cfdd403c1
johanforero77 <johanforero77@hotmail.com>
2021-04-03 23:32:20 -0500

    realizacion delas pruebas

--- src/test/java/com/mercadolibre/app/mutantes/MercadolibreMutantesApplicationTests.java
+++ src/test/java/com/mercadolibre/app/mutantes/MercadolibreMutantesApplicationTests.java
@@ -3,23 +3,30 @@ package com.mercadolibre.app.mutantes;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import com.mercadolibre.app.mutantes.controllers.AdnController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
 import com.mercadolibre.app.mutantes.models.entity.Adn;
 
 @SpringBootTest
 class MercadolibreMutantesApplicationTests {
 	
-	@Autowired
-	private AdnController dnaController;
+
 
 	@Test
 	void horizontalValidate() {
 		String[] arrayDna = {"ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"};
 		Adn dna = new Adn();
 		dna.setDna(arrayDna);
-		assertEquals(200, dnaController.mutant(dna).getStatusCodeValue());
+		HttpEntity<Adn> body = new HttpEntity<Adn>(dna);
+		ResponseEntity<Adn> response = restTemplate().exchange(
+				"http://3.131.137.77:80/mutant", 
+				HttpMethod.POST, body,  Adn.class);
+		assertEquals(200, response.getStatusCodeValue());
 	}
 	
 	@Test
@@ -27,7 +34,11 @@ class MercadolibreMutantesApplicationTests {
 		String[] arrayDna = {"ATGCGA","CAGTGC","TTATGT","AGAAGG","TCCCTA","TCACTG"};
 		Adn dna = new Adn();
 		dna.setDna(arrayDna);
-		assertEquals(200, dnaController.mutant(dna).getStatusCodeValue());
+		HttpEntity<Adn> body = new HttpEntity<Adn>(dna);
+		ResponseEntity<Adn> response = restTemplate().exchange(
+				"http://3.131.137.77:80/mutant", 
+				HttpMethod.POST, body,  Adn.class);
+		assertEquals(200, response.getStatusCodeValue());
 	}
 	
 	@Test
@@ -35,7 +46,11 @@ class MercadolibreMutantesApplicationTests {
 		String[] arrayDna = {"ATGCTA","CAGTGC","TTATGT","AGAAGG","TCCCTA","TCACTG"};
 		Adn dna = new Adn();
 		dna.setDna(arrayDna);
-		assertEquals(200, dnaController.mutant(dna).getStatusCodeValue());
+		HttpEntity<Adn> body = new HttpEntity<Adn>(dna);
+		ResponseEntity<Adn> response = restTemplate().exchange(
+				"http://3.131.137.77:80/mutant", 
+				HttpMethod.POST, body,  Adn.class);
+		assertEquals(200, response.getStatusCodeValue());
 	}
 	
 	@Test
@@ -43,15 +58,26 @@ class MercadolibreMutantesApplicationTests {
 		String[] arrayDna = {"GTGCTA","CAGTGC","TTGTGT","AGAGGG","TCCCGA","TCACTG"};
 		Adn dna = new Adn();
 		dna.setDna(arrayDna);
-		assertEquals(200, dnaController.mutant(dna).getStatusCodeValue());
+		HttpEntity<Adn> body = new HttpEntity<Adn>(dna);
+		ResponseEntity<Adn> response = restTemplate().exchange(
+				"http://3.131.137.77:80/mutant", 
+				HttpMethod.POST, body,  Adn.class);
+		assertEquals(200, response.getStatusCodeValue());
 	}
 	
 	void humanValidate() {
 		String[] arrayDna = {"GTGCTA","CAGTGC","TTATGT","AGAGGG","TCCCGA","TCACTG"};
 		Adn dna = new Adn();
 		dna.setDna(arrayDna);
-		assertEquals(403, dnaController.mutant(dna).getStatusCodeValue());
+		HttpEntity<Adn> body = new HttpEntity<Adn>(dna);
+		ResponseEntity<Adn> response = restTemplate().exchange(
+				"http://3.131.137.77:80/mutant", 
+				HttpMethod.POST, body,  Adn.class);
+		assertEquals(403, response.getStatusCodeValue());
 	}
-	
+	@Bean
+	public RestTemplate restTemplate() {
+	    return new RestTemplate();
+	}
 
 }
